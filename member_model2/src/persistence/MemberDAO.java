9de0855b33701375d8526fe9e5d26104804028f8
Jjae923 <62982316+Jjae923@users.noreply.github.com>
2020-06-24 17:27:22 +0900

    0624 수업 commit
    0624 수업 commit

--- member_model2/src/persistence/MemberDAO.java
+++ member_model2/src/persistence/MemberDAO.java
@@ -1,9 +1,9 @@
 package persistence;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -47,13 +47,15 @@ public class MemberDAO {
 	
 	// 로그인 처리 (select) 아이디와 이름이 return 되도록
 	public MemberVO isLogin(String userid, String password) {
+		// 정적쿼리	: 외부에서 입력된 데이터를 자료형에 맞게 지정된
+		//			  위치에 바인딩 시켜 쿼리를 수행하는 방식
 		String sql = "select * from member where userid = ? and password = ?";
 		
 		int result;
 		try(Connection con = getConnection();
 			PreparedStatement pstmt = con.prepareStatement(sql)) {
-				pstmt.setString(1, userid);
-				pstmt.setString(2, password);
+				pstmt.setString(1, userid); 						// 바인딩하는 부분
+				pstmt.setString(2, password);						// 바인딩하는 부분
 				ResultSet rs = pstmt.executeQuery();
 				if(rs.next()) {
 					MemberVO vo = new MemberVO();
@@ -66,6 +68,27 @@ public class MemberDAO {
 		}		
 		return null;		
 	}
+	
+	// 로그인 처리 Statement로 변경
+//	public MemberVO isLogin(String userid, String password) {
+//		// 동적쿼리 => SQL인젝션 공격에 노출될 가능성이 높음
+//		String sql = "select userid,name from member where userid='"+userid+"' and password='"+password+"'"; // 물음표 못씀 --> pstmt.set 사라짐
+//		
+//		int result;
+//		try(Connection con = getConnection();
+//			Statement pstmt = con.createStatement()) { // sql 바로 못감
+//				ResultSet rs = pstmt.executeQuery(sql); // executeQuery로 sql 날림
+//				if(rs.next()) {
+//					MemberVO vo = new MemberVO();
+//					vo.setUserid(rs.getString("userid"));
+//					vo.setName(rs.getString("name"));
+//					return vo;
+//				}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}		
+//		return null;		
+//	}
 	
 	// 비밀번호 변경
 	public int passwordUpdate(String userid, String new_password, String current_password) {
